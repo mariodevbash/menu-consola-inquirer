ab8414a5ae2c64e88d491ca236c00687cfa40b31
Mario Ochoa <mario120gdl@gmail.com>
2022-02-01 18:26:28 -0600

    Primer commit, proyecto casi finalizado

---
+++ models/tareas.js
@@ -0,0 +1,80 @@
+const Tarea = require('./tarea');
+const colors = require('colors')
+
+class Tareas {
+    _listado = {};
+
+    get listadoArr() {
+
+        const listado = [];
+
+        Object.keys(this._listado).forEach(key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        });
+
+        return listado;
+    }
+
+    constructor() {
+        this._listado = {};
+    }
+
+    borrarTarea(id = '') {
+        if (this._listado[id]) {
+            delete this._listado[id];
+        }
+    }
+
+    cargarTareasFromArray(tareas = []) {
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        })
+    }
+
+    crearTarea(desc = '') {
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
+    }
+
+    listadoCompleto() {
+        console.log();
+        this.listadoArr.forEach((tarea, i) => {
+
+            const index = `${i + 1}. `.green;
+            const { desc, completadoEn } = tarea;
+            const estado = (completadoEn)
+                ? 'Completada'.green
+                : 'Pendiente'.red;
+
+            console.log(`${index} ${desc} :: ${estado}`);
+            // console.log(tarea.desc);
+        });
+    }
+
+    listarPendientesCompletadas(completadas = true) {
+        let contador = 0;
+
+        this.listadoArr.forEach(tarea => {
+
+            const { desc, completadoEn } = tarea;
+            const estado = (completadoEn)
+                ? 'Completada'.green
+                : 'Pendiente'.red;
+
+            if (completadas) {
+                if (completadoEn) {
+                    contador += 1;
+                    console.log(`${(contador + '.').green} ${desc} :: ${completadoEn}`);
+                }
+            } else {
+                if (!completadoEn) {
+                    contador += 1;
+                    console.log(`${(contador + '.').green} ${desc} :: ${estado}`);
+                }
+            }
+        });
+    }
+}
+
+module.exports = Tareas;
